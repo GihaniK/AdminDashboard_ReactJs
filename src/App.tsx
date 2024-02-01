68481a06d5a0f4740bdaf5dd58fef2bc416c0956
GihaniK <gihani.k.welabadage@gmail.com>
2024-02-01 12:09:42 +0530

    Added : Components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
 import Home from "./pages/home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
 
 function App() {
 
-  return (
-    <div><Home/></div>
-  )
+  const Layout = ()=>{
+    return(
+      <div className="main">
+        
+      </div>
+    )
+  }
+
+
+
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home />
+      ),
+    },
+    {
+      path: "users",
+      element: <Users/>,
+    },
+    {
+      path: "products",
+      element: <Products/>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
+ 
